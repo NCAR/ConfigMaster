ba12fb96718d3bbdf90331b1e2482479bea6dc8e
Paul Prestopnik <prestop@rap.ucar.edu>
2018-09-14 14:53:00 -0600

    initial commit - copied from CVS HEAD on 2018-09-14.  Updated to python3.

---
+++ example1/myparams.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+#####################################
+## GENERAL CONFIGURATION
+#####################################
+ 
+## debug ##
+# Flag to output debugging information
+debug = False
+
+# Forecast hour
+forecasthour = 10
+
+# Email Address
+emailAddy = "prestop@ucar.edu"
+ 
+
